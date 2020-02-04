2dff0e0585cf972d0bc76df1cf2624259dce1ddf
FabritsievD <fabritsiev_dv@ukr.net>
2020-02-04 08:51:23 +0200

    initial commit

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+import com.example.demo.model.DemoResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Controller {
+    @Autowired
+    DemoResponse demoResponse;
+
+    @RequestMapping("/")
+    public DemoResponse getMessage() {
+        demoResponse.setValue("World");
+        return demoResponse;
+    }
+}
